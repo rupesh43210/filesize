2c91bfdbea2c9127486c9ab46422ff4248e7bcdd
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2023-04-03 16:09:11 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,38 @@
-from setuptools import setup
+import os
+import sys
+import inspect
 
-setup(
-    name='filesize',
-    version='1.0',
-    description='A tool for checking file sizes',
-    author='Your Name',
-    author_email='rupesh4321@gmail.com',
-    url='https://github.com/rupesh43210/filesize',
-    packages=['filesize'],
-    install_requires=[
-        'tabulate',
-        'termcolor',
-    ],
-    entry_points={
-        'console_scripts': [
-            'filesize = filesize.file_size:main',
-        ],
-    },
-)
+def install_dependencies():
+    try:
+        os.system("which pip3 || sudo apt-get install python3-pip")
+        os.system("pip3 install tabulate termcolor")
+        print("\nPackages installed successfully.")
+    except Exception as e:
+        print(f"Error installing packages: {e}")
+        sys.exit(1)
+
+def create_alias():
+    try:
+        shell = os.environ['SHELL']
+        if 'bash' in shell:
+            config_file = os.path.expanduser('~/.bashrc')
+        elif 'zsh' in shell:
+            config_file = os.path.expanduser('~/.zshrc')
+        else:
+            print("Unsupported shell. Please add the alias manually.")
+            return
+
+        path = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        file_size_path = os.path.join(path, 'file_size.py')
+
+        with open(config_file, 'a') as f:
+            f.write(f'\nalias filesize="sudo python3 {file_size_path}"\n')
+
+        print("\nAlias 'filesize' created successfully.")
+    except Exception as e:
+        print(f"Error creating alias: {e}")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    install_dependencies()
+    create_alias()

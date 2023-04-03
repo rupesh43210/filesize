ac8e1cffdec8f3baa9d8fe44dc2fe0be0bf65ffe
Rupesh <85641027+rupesh43210@users.noreply.github.com>
2023-04-03 15:56:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,20 @@
 from setuptools import setup
 
 setup(
-    name='file_size',
-    version='0.1',
+    name='filesize',
+    version='1.0',
+    description='A tool for checking file sizes',
     author='Your Name',
-    author_email='your.email@example.com',
-    description='A package to calculate file size from any location',
-    url='https://github.com/your-username/file_size',
-    py_modules=['file_size', 'functions'],
-    install_requires=['tabulate', 'termcolor'],
+    author_email='rupesh4321@gmail.com',
+    url='https://github.com/rupesh43210/filesize',
+    packages=['filesize'],
+    install_requires=[
+        'tabulate',
+        'termcolor',
+    ],
     entry_points={
         'console_scripts': [
-            'filesize = file_size:main'
-        ]
-    }
+            'filesize = filesize.file_size:main',
+        ],
+    },
 )
